56d8eca62f2c541cd8adf3684a4f3b58b266f3ea
romainniamor <romainnavoret@gmail.com>
2023-05-28 16:52:01 +0200

    contact front done

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("test");
+//black wrappers sliding
+
+gsap.from(".hero-image-wrapper, .content-wrapper, .front-img", 2, {
+  delay: 1.5,
+  clipPath: "inset(0 100% 0 0)",
+  ease: "power4.inOut",
+  stagger: {
+    amount: 0.5,
+  },
+});
+
+// zoom out on img
+
+gsap.from("img", 2, {
+  delay: 2,
+  scale: 2,
+  ease: "power4.inOut",
+  stagger: {
+    amount: 0.25,
+  },
+});
+
+// vert slide h1 & h2
+
+gsap.to("header h1, header h2", 1, {
+  delay: 2.5,
+  top: 0,
+  ease: "power3.out",
+  stagger: {
+    amount: 1,
+  },
+});
